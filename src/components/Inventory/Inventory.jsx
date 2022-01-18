4e6433ca609b667a03605c021f6059d3120b6866
AllenJoeG <allen.joeg@gmail.com>
2022-01-18 11:53:51 -0600

    Updated green odd-nth row table color to match Pack view. Argh

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -25,7 +25,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
+    backgroundColor: '#E8F5E9'
   },
   // hide last border
   '&:last-child td, &:last-child th': {
